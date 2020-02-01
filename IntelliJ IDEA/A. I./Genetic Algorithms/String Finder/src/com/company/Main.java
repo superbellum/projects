1f49419d1cfda0d467da93d1e746ee709c071fe3
gughpatrik619 <gugh.patrik@gmail.com>
2020-02-01 21:57:35 +0100

    - IoC and DI finished;

---
+++ IntelliJ IDEA/A. I./Genetic Algorithms/String Finder/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+
+public class Main
+{
+    public static void main(String[] args)
+	{
+		Processing processing = new Processing();
+
+		processing.setup();
+
+		processing.draw();
+	}
+}
